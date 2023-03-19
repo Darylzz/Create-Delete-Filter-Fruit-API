f376e1576436c9f54a71b2eb8b0faf4987b80abd
Darylzz <cheer_bo50@hotmail.com>
2023-03-20 04:31:16 +0700

    success feature

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const notFoundMiddleware = require("./middlewares/notFound");
 const errorMiddleware = require("./middlewares/error");
 const fruitRoute = require("./routes/FruitRoute");
@@ -7,7 +8,10 @@ const fruitRoute = require("./routes/FruitRoute");
 // sequelize.sync({ force: true });
 
 const app = express();
+app.use("/public", express.static("public"));
 app.use(express.json());
+
+app.use(cors());
 
 app.use("/fruit", fruitRoute);
 
